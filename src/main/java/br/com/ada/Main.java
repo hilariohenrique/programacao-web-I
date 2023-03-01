c0066f91841a0d6a87251113901b8acbdf913352
hilariohenrique <hilariohenr@hotmail.com>
2023-03-01 17:28:22 -0300

    --

--- src/main/java/br/com/ada/Main.java
+++ src/main/java/br/com/ada/Main.java
@@ -9,14 +9,40 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/translate")
+@Path("/service")
 public class Main {
     @Inject
     private TranslateService translateService;
+
+    @Path("/translate")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return translateService.getMessage("de");
+    public String helloTranslate() {
+        return translateService.getMessage("en");
     }
 
+    @Path("/carro")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String carro() {
+        return new Carro("Hillux","LKJ7986", "Toyota").toString();
+    }
+
+}
+
+class Carro {
+    private String modelo;
+    private String placa;
+    private String marca;
+
+    public Carro(String modelo, String placa, String marca) {
+        this.modelo = modelo;
+        this.placa = placa;
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return this.placa+" - "+this.modelo+" - "+this.marca;
+    }
 }
